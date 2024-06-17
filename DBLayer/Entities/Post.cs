4c96ec6a3c44bb01418030bba804128c73ba9acc
aluisaoliveira <aluisaoliveira@ipvc.pt>
2024-06-17 23:37:15 +0100

    Entities

---
+++ DBLayer/Entities/Post.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ESOF.WebApp.DBLayer.Entities;
+
+public class Post
+{
+    [Key]
+    public Guid PostId { get; set; }
+    
+    [Required]
+    public string comment { get; set; }
+    
+    [Required]
+    public string title { get; set; }
+    
+    public User User { get; set; }
+    
+    public Game Game { get; set; }
+    
+    
+}
---
+++ DBLayer/Entities/Post_like.cs
@@ -0,0 +1,8 @@
+﻿namespace ESOF.WebApp.DBLayer.Entities;
+
+public class Post_like
+{
+    public User User { get; set; } 
+    
+    public Post Post { get; set; } 
+}
