60312a995f45c75784679f15645eef14716c66c4
Rui Alves <ruijorge02012001@gmail.com>
2024-06-20 11:04:03 +0100

    favorirtes listing

--- Frontend/Helpers/ApiHelper.cs
+++ Frontend/Helpers/ApiHelper.cs
@@ -16,21 +16,6 @@ public class ApiHelper(HttpClient httpClient)
             throw new ApplicationException($"Error fetching data from {url}: {e.Message}");
         }
     }
-    /*public async Task<T?> PostToApiAsync<T>(string url, T data)
-    {
-        try
-        {
-            var response = await httpClient.PostAsJsonAsync(url, data);
-            response.EnsureSuccessStatusCode();
-            return await response.Content.ReadFromJsonAsync<T>();
-        }
-        catch (HttpRequestException e)
-        {
-            // Handle exception
-            throw new ApplicationException($"Error posting data to {url}: {e.Message}");
-        }
-    }*/
-
     public async Task<T?> PostToApiAsync<T>(string url)
     {
         try
