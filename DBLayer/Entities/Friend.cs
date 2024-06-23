15ff3a3d4603bb303fa67dd9ebfbb26ab8dade5b
gabrielaribeiro <gabrielatr200@gmail.com>
2024-06-23 01:22:39 +0100

    Friend.cs - Change : Added attributes to friend entity

--- DBLayer/Entities/Friend.cs
+++ DBLayer/Entities/Friend.cs
@@ -2,5 +2,11 @@ namespace ESOF.WebApp.DBLayer.Entities;
 
 public class Friend
 {
-    public ICollection<UserRole> FriendUserRole { get; set; }
+    public int Id { get; set; }
+    public Guid SenderId { get; set; }
+    public User Sender { get; set; }
+    public Guid ReceiverId { get; set; }
+    public User Receiver { get; set; }
+    public DateTime RequestDate { get; set; }
+    
 }
