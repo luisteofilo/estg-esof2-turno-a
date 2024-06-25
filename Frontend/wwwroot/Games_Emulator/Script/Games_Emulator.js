78ec735409b84cf47d530b4689eea9f8c1944ac5
migcruzz <cruz.miguel@ipvc.pt>
2024-06-25 11:07:10 +0100

    java script emulator updated

--- Frontend/wwwroot/Games_Emulator/Script/Games_Emulator.js
+++ Frontend/wwwroot/Games_Emulator/Script/Games_Emulator.js
@@ -1,14 +1,20 @@
 
+
+function emulator_trigger(url_cs, name_cs){
+
+    // Info Recieved from C#
+
+    const url = URL.createObjectURL(url_cs);
+
+    const parts = name_cs.split(".");
+    const file_name = name_cs.name;
+
     console.log("Estou aqui no inicio");
 
     let enableDebug = true;
     let enableThreads = false;
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
-    
-    console.log(`Query string é ${queryString}`);
-    console.log(`URL Params é ${urlParams}`);
-    
     if (urlParams.get("debug") == 1) {
         enableDebug = true;
         console.log("Debug is enabled");
@@ -30,159 +36,334 @@
         console.log("Threads are disabled");
     }
 
-    input.onchange = async () => {
-        const url = URL.createObjectURL(input.files[0]);
-        console.log(`O URL é ${url}`);
-        const parts = input.files[0].name.split(".");
-        const file_name = input.files[0].name;
 
-        //let blob = await fetch(url).then(r => r.blob());
-        
-        console.log(parts);
-        console.log(file_name);
-        //console.log(blob);
 
-        const core = await (async (ext) => {
-            if (["fds", "nes", "unif", "unf"].includes(ext)) return "nes";
 
-            if (
-                ["smc", "fig", "sfc", "gd3", "gd7", "dx2", "bsx", "swc"].includes(ext)
-            )
-                return "snes";
+    console.log(parts);
+    console.log(file_name);
+    console.log(url);
 
-            if (["z64", "n64"].includes(ext)) return "n64";
+    const core = await (async (ext) => {
+        if (["fds", "nes", "unif", "unf"].includes(ext)) return "nes";
 
-            if (["pce"].includes(ext)) return "pce";
+        if (
+            ["smc", "fig", "sfc", "gd3", "gd7", "dx2", "bsx", "swc"].includes(ext)
+        )
+            return "snes";
 
-            if (["ngp", "ngc"].includes(ext)) return "ngp";
+        if (["z64", "n64"].includes(ext)) return "n64";
 
-            if (["ws", "wsc"].includes(ext)) return "ws";
+        if (["pce"].includes(ext)) return "pce";
 
-            if (["col", "cv"].includes(ext)) return "coleco";
+        if (["ngp", "ngc"].includes(ext)) return "ngp";
 
-            if (["d64"].includes(ext)) return "vice_x64sc";
+        if (["ws", "wsc"].includes(ext)) return "ws";
 
-            if (["nds", "gba", "gb", "z64", "n64"].includes(ext)) return ext;
+        if (["col", "cv"].includes(ext)) return "coleco";
 
-            return await new Promise((resolve) => {
-                var coreValues = {
-                    "Nintendo 64": "n64",
-                    "Nintendo Game Boy": "gb",
-                    "Nintendo Game Boy Advance": "gba",
-                    "Nintendo DS": "nds",
-                    "Nintendo Entertainment System": "nes",
-                    "Super Nintendo Entertainment System": "snes",
-                    PlayStation: "psx",
-                    "Virtual Boy": "vb",
-                    "Sega Mega Drive": "segaMD",
-                    "Sega Master System": "segaMS",
-                    "Sega CD": "segaCD",
-                    "Atari Lynx": "lynx",
-                    "Sega 32X": "sega32x",
-                    "Atari Jaguar": "jaguar",
-                    "Sega Game Gear": "segaGG",
-                    "Sega Saturn": "segaSaturn",
-                    "Atari 7800": "atari7800",
-                    "Atari 2600": "atari2600",
-                    "NEC TurboGrafx-16/SuperGrafx/PC Engine": "pce",
-                    "NEC PC-FX": "pcfx",
-                    "SNK NeoGeo Pocket (Color)": "ngp",
-                    "Bandai WonderSwan (Color)": "ws",
-                    ColecoVision: "coleco",
-                    "Commodore 64": "vice_x64sc",
-                    "Commodore 128": "vice_x128",
-                    "Commodore VIC20": "vice_xvic",
-                    "Commodore Plus/4": "vice_xplus4",
-                    "Commodore PET": "vice_xpet",
-                };
+        if (["d64"].includes(ext)) return "vice_x64sc";
 
-                const cores = Object.keys(coreValues)
-                    .sort()
-                    .reduce((obj, key) => {
-                        obj[key] = coreValues[key];
-                        return obj;
-                    }, {});
+        if (["nds", "gba", "gb", "z64", "n64"].includes(ext)) return ext;
 
-                const button = document.createElement("button");
-                const select = document.createElement("select");
+        return await new Promise((resolve) => {
+            var coreValues = {
+                "Nintendo 64": "n64",
+                "Nintendo Game Boy": "gb",
+                "Nintendo Game Boy Advance": "gba",
+                "Nintendo DS": "nds",
+                "Nintendo Entertainment System": "nes",
+                "Super Nintendo Entertainment System": "snes",
+                PlayStation: "psx",
+                "Virtual Boy": "vb",
+                "Sega Mega Drive": "segaMD",
+                "Sega Master System": "segaMS",
+                "Sega CD": "segaCD",
+                "Atari Lynx": "lynx",
+                "Sega 32X": "sega32x",
+                "Atari Jaguar": "jaguar",
+                "Sega Game Gear": "segaGG",
+                "Sega Saturn": "segaSaturn",
+                "Atari 7800": "atari7800",
+                "Atari 2600": "atari2600",
+                "NEC TurboGrafx-16/SuperGrafx/PC Engine": "pce",
+                "NEC PC-FX": "pcfx",
+                "SNK NeoGeo Pocket (Color)": "ngp",
+                "Bandai WonderSwan (Color)": "ws",
+                ColecoVision: "coleco",
+                "Commodore 64": "vice_x64sc",
+                "Commodore 128": "vice_x128",
+                "Commodore VIC20": "vice_xvic",
+                "Commodore Plus/4": "vice_xplus4",
+                "Commodore PET": "vice_xpet",
+            };
 
-                for (const type in cores) {
-                    const option = document.createElement("option");
+            const cores = Object.keys(coreValues)
+                .sort()
+                .reduce((obj, key) => {
+                    obj[key] = coreValues[key];
+                    return obj;
+                }, {});
 
-                    option.value = cores[type];
-                    option.textContent = type;
-                    select.appendChild(option);
-                }
+            const button = document.createElement("button");
+            const select = document.createElement("select");
 
-                button.onclick = () => resolve(select[select.selectedIndex].value);
-                button.textContent = "Load game";
-                box.innerHTML = "";
+            for (const type in cores) {
+                const option = document.createElement("option");
 
-                box.appendChild(select);
-                box.appendChild(button);
-            });
-        })(parts.pop());
+                option.value = cores[type];
+                option.textContent = type;
+                select.appendChild(option);
+            }
 
-        const div = document.createElement("div");
-        const sub = document.createElement("div");
-        const script = document.createElement("script");
+            button.onclick = () => resolve(select[select.selectedIndex].value);
+            button.textContent = "Load game";
+            box.innerHTML = "";
 
-        sub.id = "game";
-        div.id = "display";
+            box.appendChild(select);
+            box.appendChild(button);
+        });
+    })(parts.pop());
 
-        const top = document.getElementById("top");
-        top.remove();
-        box.remove();
-        div.appendChild(sub);
-        document.body.appendChild(div);
+    const div = document.createElement("div");
+    const sub = document.createElement("div");
+    const script = document.createElement("script");
 
-        // Configurações do EmulatorJS
-        window.EJS_player = "#game";
-        window.EJS_gameName = parts.join(".");
-        window.EJS_biosUrl = "";
-        window.EJS_gameUrl = url;
-        window.EJS_core = core;
-        window.EJS_pathtodata = "https://cdn.emulatorjs.org/stable/data/";
-        window.EJS_startOnLoaded = false;
-        window.EJS_DEBUG_XX = enableDebug;
-        window.EJS_disableDatabases = true;
-        window.EJS_threads = enableThreads;
-        window.EJS_fullscreenOnLoaded = false;
-        // window.EJS_loadStateURL = url; // Define a URL de carregamento do estado salvo
+    sub.id = "game";
+    div.id = "display";
 
-        EJS_Buttons = {
-            playPause: true,
-            restart: true,
-            mute: true,
-            settings: false,
-            fullscreen: true,
-            saveState: true,
-            loadState: true,
-            screenRecord: true,
-            gamepad: true,
-            cheat: false,
-            volume: true,
-            saveSavFiles: true,
-            loadSavFiles: true,
-            quickSave: false,
-            quickLoad: false,
-            screenshot: true,
-            cacheManager: false,
-        };
+    const top = document.getElementById("top");
+    top.remove();
+    box.remove();
+    div.appendChild(sub);
+    document.body.appendChild(div);
 
-        script.src = "https://cdn.emulatorjs.org/stable/data/loader.js";
-        document.body.appendChild(script);
+    // Configurações do EmulatorJS
+    window.EJS_player = "#game";
+    window.EJS_gameName = parts.join(".");
+    window.EJS_biosUrl = "";
+    window.EJS_gameUrl = url;
+    window.EJS_core = core;
+    window.EJS_pathtodata = "https://cdn.emulatorjs.org/stable/data/";
+    window.EJS_startOnLoaded = false;
+    window.EJS_DEBUG_XX = enableDebug;
+    window.EJS_disableDatabases = true;
+    window.EJS_threads = enableThreads;
+    window.EJS_fullscreenOnLoaded = false;
+    // window.EJS_loadStateURL = url; // Define a URL de carregamento do estado salvo
 
-        // Mostrar controles de estado salvo
-        document.getElementById("controls").style.display = "flex";
+    EJS_Buttons = {
+        playPause: true,
+        restart: true,
+        mute: true,
+        settings: false,
+        fullscreen: true,
+        saveState: true,
+        loadState: true,
+        screenRecord: true,
+        gamepad: true,
+        cheat: false,
+        volume: true,
+        saveSavFiles: true,
+        loadSavFiles: true,
+        quickSave: false,
+        quickLoad: false,
+        screenshot: true,
+        cacheManager: false,
     };
-    
+
+    script.src = "https://cdn.emulatorjs.org/stable/data/loader.js";
+    document.body.appendChild(script);
+
+    // Mostrar controles de estado salvo
+    document.getElementById("controls").style.display = "flex";
+
 
     box.ondragover = () => box.setAttribute("drag", true);
     box.ondragleave = () => box.removeAttribute("drag");
 
 
+}
 
+
+
+
+
+
+
+console.log("Estou aqui no inicio");
+
+let enableDebug = true;
+let enableThreads = false;
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+if (urlParams.get("debug") == 1) {
+    enableDebug = true;
+    console.log("Debug is enabled");
+} else {
+    console.log("Debug is disabled");
+}
+
+if (urlParams.get("threads") == 1) {
+    if (window.SharedArrayBuffer) {
+        enableThreads = true;
+        console.log("Threads are enabled");
+    } else {
+        console.warn(
+            "Threads are disabled as SharedArrayBuffer is not available. Threads requires two headers to be set when sending you html page. See https://stackoverflow.com/a/68630724"
+        );
+        console.log("Threads are disabled");
+    }
+} else {
+    console.log("Threads are disabled");
+}
+
+input.onchange = async () => {
+    const url = URL.createObjectURL(input.files[0]);
+
+    const parts = input.files[0].name.split(".");
+    const file_name = input.files[0].name;
+
+    console.log(parts);
+    console.log(file_name);
+    console.log(url);
+
+    const core = await (async (ext) => {
+        if (["fds", "nes", "unif", "unf"].includes(ext)) return "nes";
+
+        if (
+            ["smc", "fig", "sfc", "gd3", "gd7", "dx2", "bsx", "swc"].includes(ext)
+        )
+            return "snes";
+
+        if (["z64", "n64"].includes(ext)) return "n64";
+
+        if (["pce"].includes(ext)) return "pce";
+
+        if (["ngp", "ngc"].includes(ext)) return "ngp";
+
+        if (["ws", "wsc"].includes(ext)) return "ws";
+
+        if (["col", "cv"].includes(ext)) return "coleco";
+
+        if (["d64"].includes(ext)) return "vice_x64sc";
+
+        if (["nds", "gba", "gb", "z64", "n64"].includes(ext)) return ext;
+
+        return await new Promise((resolve) => {
+            var coreValues = {
+                "Nintendo 64": "n64",
+                "Nintendo Game Boy": "gb",
+                "Nintendo Game Boy Advance": "gba",
+                "Nintendo DS": "nds",
+                "Nintendo Entertainment System": "nes",
+                "Super Nintendo Entertainment System": "snes",
+                PlayStation: "psx",
+                "Virtual Boy": "vb",
+                "Sega Mega Drive": "segaMD",
+                "Sega Master System": "segaMS",
+                "Sega CD": "segaCD",
+                "Atari Lynx": "lynx",
+                "Sega 32X": "sega32x",
+                "Atari Jaguar": "jaguar",
+                "Sega Game Gear": "segaGG",
+                "Sega Saturn": "segaSaturn",
+                "Atari 7800": "atari7800",
+                "Atari 2600": "atari2600",
+                "NEC TurboGrafx-16/SuperGrafx/PC Engine": "pce",
+                "NEC PC-FX": "pcfx",
+                "SNK NeoGeo Pocket (Color)": "ngp",
+                "Bandai WonderSwan (Color)": "ws",
+                ColecoVision: "coleco",
+                "Commodore 64": "vice_x64sc",
+                "Commodore 128": "vice_x128",
+                "Commodore VIC20": "vice_xvic",
+                "Commodore Plus/4": "vice_xplus4",
+                "Commodore PET": "vice_xpet",
+            };
+
+            const cores = Object.keys(coreValues)
+                .sort()
+                .reduce((obj, key) => {
+                    obj[key] = coreValues[key];
+                    return obj;
+                }, {});
+
+            const button = document.createElement("button");
+            const select = document.createElement("select");
+
+            for (const type in cores) {
+                const option = document.createElement("option");
+
+                option.value = cores[type];
+                option.textContent = type;
+                select.appendChild(option);
+            }
+
+            button.onclick = () => resolve(select[select.selectedIndex].value);
+            button.textContent = "Load game";
+            box.innerHTML = "";
+
+            box.appendChild(select);
+            box.appendChild(button);
+        });
+    })(parts.pop());
+
+    const div = document.createElement("div");
+    const sub = document.createElement("div");
+    const script = document.createElement("script");
+
+    sub.id = "game";
+    div.id = "display";
+
+    const top = document.getElementById("top");
+    top.remove();
+    box.remove();
+    div.appendChild(sub);
+    document.body.appendChild(div);
+
+    // Configurações do EmulatorJS
+    window.EJS_player = "#game";
+    window.EJS_gameName = parts.join(".");
+    window.EJS_biosUrl = "";
+    window.EJS_gameUrl = url;
+    window.EJS_core = core;
+    window.EJS_pathtodata = "https://cdn.emulatorjs.org/stable/data/";
+    window.EJS_startOnLoaded = false;
+    window.EJS_DEBUG_XX = enableDebug;
+    window.EJS_disableDatabases = true;
+    window.EJS_threads = enableThreads;
+    window.EJS_fullscreenOnLoaded = false;
+    // window.EJS_loadStateURL = url; // Define a URL de carregamento do estado salvo
+
+    EJS_Buttons = {
+        playPause: true,
+        restart: true,
+        mute: true,
+        settings: false,
+        fullscreen: true,
+        saveState: true,
+        loadState: true,
+        screenRecord: true,
+        gamepad: true,
+        cheat: false,
+        volume: true,
+        saveSavFiles: true,
+        loadSavFiles: true,
+        quickSave: false,
+        quickLoad: false,
+        screenshot: true,
+        cacheManager: false,
+    };
+
+    script.src = "https://cdn.emulatorjs.org/stable/data/loader.js";
+    document.body.appendChild(script);
+
+    // Mostrar controles de estado salvo
+    document.getElementById("controls").style.display = "flex";
+};
+
+box.ondragover = () => box.setAttribute("drag", true);
+box.ondragleave = () => box.removeAttribute("drag");
 
 
  
