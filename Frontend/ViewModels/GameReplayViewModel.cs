6aaeb50b44097722412b8d4a53265c06a0d76b06
diogo012 <diogorosas@ipvc.pt>
2024-06-25 13:48:33 +0100

    ControllersAndViewModels

---
+++ Frontend/ViewModels/GameReplayViewModel.cs
@@ -0,0 +1,10 @@
+namespace Frontend.ViewModels;
+
+public class GameReplayViewModel
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string FilePath { get; set; }
+    public DateTime UploadDate { get; set; }
+    public byte[] VideoData { get; set; }
+}
