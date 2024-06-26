a5d4032084c009b4b337fabcf7ed3c62084f0caf
Nelson Freitas <tozefernandes@msn.com>
2024-06-26 11:54:52 +0100

    Request methods to a Controller folder

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -9,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -51,36 +52,6 @@ app.MapGet("/users/emails", () =>
 
 
 /*GET ALL ACHIEVEMENT LIST*/
-app.MapGet("/achievements", () =>
-    {
-        var db = new ApplicationDbContext();
-        return db.Achievements.Select(a => new AchievementsViewModel()
-        {
-            Name = a.Name,
-            Description = a.Description,
-            RequiredScore = a.RequiredScore
-        }).ToArray();
-    })
-    .WithName("GetAchievements")
-    .WithOpenApi();
-
-app.MapGet("/UserAchievements/{userId:Guid}", (Guid userId) =>
-    {
-        var db = new ApplicationDbContext();
-        var user_achievements =  (from pa in db.PlayerAchievements
-            join a in db.Achievements on pa.AchievementId equals a.IdAchievement
-            where pa.UserId == userId
-            select new PlayerAchievementsViewModel
-            {
-                Name = a.Name,
-                Description = a.Description,
-                Unlocked = pa.Unlocked,
-                AchievementId = pa.AchievementId
-            }).ToArray();
-        return user_achievements;
-    })
-    .WithName("GetUserAchievements")
-    .WithOpenApi();
 
 app.MapGet("/users", () =>
     {
@@ -94,69 +65,7 @@ app.MapGet("/users", () =>
     .WithName("GetUsers")
     .WithOpenApi();
 
-app.MapPost("/SaveScore/{userId:Guid}/{Score:long}", async (Guid userId, long score) =>
-    {
-        //var request = await context.Request.ReadFromJsonAsync<ScoreViewModel>();
-        var db = new ApplicationDbContext();
-
-        var scoreEntry = new TestUserScore
-        {
-            UserId = userId,
-            Score = score
-        };
-
-        db.TestUserScores.Add(scoreEntry);
-        await db.SaveChangesAsync();
-        return Results.Ok(new { message = "Achievement added successfully." });
-        //context.Response.StatusCode = StatusCodes.Status200OK;
-        //await context.Response.WriteAsync("Score saved successfully.");
-    }).WithName("SaveScore")
-    .WithOpenApi();
-
-app.MapGet("/ScoreAchievements/{score:long}", (long score) =>
-    {
-        var db = new ApplicationDbContext();
-        var achievements =  (from a in db.Achievements
-            where a.RequiredScore <= score
-            select new AchievementsViewModel()
-            {
-                IdAchievement = a.IdAchievement,
-                Name = a.Name,
-                Description = a.Description
-            }).ToArray();
-        return achievements;
-    })
-    .WithName("GetScoreAchievements")
-    .WithOpenApi();
-
-app.MapPost("/PlayerAchievements/{userId:Guid}/{achievementId:Guid}", async (Guid userId, Guid achievementId) =>
-    {
-        var db = new ApplicationDbContext();
-        // Verifica se já existe um registo com o mesmo UserId e AchievementId
-        var existingAchievement = await db.PlayerAchievements
-            .AnyAsync(pa => pa.UserId == userId && pa.AchievementId == achievementId);
-
-        if (existingAchievement)
-        {
-            return Results.BadRequest("Achievement already exists for this user.");
-        }
-
-        // Cria um novo registo
-        var newAchievement = new PlayerAchievement()
-        {
-            UserId = userId,
-            AchievementId = achievementId,
-            Unlocked = DateOnly.FromDateTime(DateTime.Now)
-        };
-
-        db.PlayerAchievements.Add(newAchievement);
-        await db.SaveChangesAsync();
-
-        return Results.Ok(new { message = "Achievement added successfully." });
-    })
-    .WithName("InsertPlayerAchievements")
-    .WithOpenApi();
-
+app.MapControllers();
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
